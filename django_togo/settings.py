89cd3ba5cd7a42cb4e0d0efd22f92e868c9af9af
EwanColquhoun <ewancolquhoun@hotmail.com>
2021-10-06 10:31:52 +0000

    Removed secret key

--- django_togo/settings.py
+++ django_togo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECREY_KEY', 'django-insecure-965^5shbyrgo52w8wmoyn)xm*3s%m+km_ygpv1)3l@4$**7f1z')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
@@ -92,8 +92,6 @@ else:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
